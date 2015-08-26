719fbc1c4ab4fd1e2e73886ba5aa5afe4e14f396
sestefan <sestefan@ntsestefan>
2015-08-26 01:47:42 -0300

    Revert "Se sube documentación de cada método"
    This reverts commit ac0fb90f41313685c2461e9a56c72b05c299fe3d.

--- ServidorCentral/src/interfaces/IControladorCategoria.java
+++ ServidorCentral/src/interfaces/IControladorCategoria.java
@@ -10,24 +10,13 @@ import java.util.ArrayList;
 public interface IControladorCategoria {
 
     /**
-     * Impacta la nueva categoría en el sistema
+     * El sistema recuerda la agrupación de datos en un DataCategoria
      *
      * @param nombre
      */
     public void altaCategoria(String nombre);
 
-    /**
-     * Devuelve true si una categoría de nombre 'nombre' existe en el sistema.
-     *
-     * @param nombre
-     * @return
-     */
     public boolean existeCategoria(String nombre);
 
-    /**
-     * Retorna una lista con las categorías existentes en el sistema
-     *
-     * @return
-     */
     public ArrayList<DataCategoria> listarCategorias();
 }
--- ServidorCentral/src/interfaces/IControladorUsuario.java
+++ ServidorCentral/src/interfaces/IControladorUsuario.java
@@ -12,78 +12,24 @@ import java.util.Date;
  */
 public interface IControladorUsuario {
 
-    /**
-     * Carga en memoria los datos de un cliente ingresados por el usuario.
-     *
-     * @param nickname
-     * @param mail
-     * @param nombre
-     * @param pass
-     * @param direccion
-     * @param apellido
-     * @param fechaNacimiento
-     * @param rutaImagen
-     */
     public void CargarDatosUsuario(String nickname, String mail, String nombre, String pass, DataDireccion direccion, String apellido, Date fechaNacimiento, String rutaImagen);
 
-    /**
-     * Carga en memoria los datos de un restaurante ingresados por el usuario.
-     *
-     * @param nickname
-     * @param mail
-     * @param nombre
-     * @param pass
-     * @param direccion
-     * @param rutaImagen
-     */
     public void CargarDatosUsuario(String nickname, String mail, String nombre, String pass, DataDireccion direccion, String[] rutaImagen);
 
-    /**
-     * Agrega a la memoria una categoria
-     *
-     * @param name
-     */
     public void seleccionarCategoria(String name);
 
-    /**
-     * Valida si el usuario con nickname @nickname existe.
-     *
-     * @param nickname
-     * @return
-     */
     public boolean existeUsuario(String nickname);
 
-    /**
-     * Valida si el usuario con nickname
-     *
-     * @param nickname
-     * @param mail
-     * @return
-     */
+    public boolean existeMail(String mail);
+
     public boolean existeUsuario(String nickname, String mail);
 
-    /**
-     * Impacta los datos cargados en memoria en el sistema.
-     */
     public void altaUsuario();
 
-    /**
-     * Limpia la memoria de los datos cargados.
-     */
     public void limpiarMemoria();
 
-    /**
-     * Retorna una lista de los clientes existentes en el sistema.
-     *
-     * @return
-     */
     public ArrayList<DataCliente> listarClientes();
 
-    /**
-     * Retorna una lista de los Restaurantes existenes en el sistema
-     *
-     * @return
-     */
     public ArrayList<DataRestaurante> listarRestaurantes();
 
 }
