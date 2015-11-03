08dd45fcc7f3df01defe32930008ad458fa74fbe
Mathi <Mathi@Mathi-Win>
2015-11-03 21:36:32 -0200

    Proyecto QOMobile

---
+++ QOMobile/src/qomobile/QOMobile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qomobile;
+
+import GUI.QOMobileGUI;
+
+/**
+ *
+ * @author Mathi
+ */
+public class QOMobile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new QOMobileGUI().setVisible(true);
+    }
+    
+}
